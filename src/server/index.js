480e19370d4a47998d56f8cbf4d03c4dcc31fe4f
Abigail Unwin <abigailunwin@gmail.com>
2022-07-06 15:27:56 +0100

    Add files via upload

---
+++ src/server/index.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const apiKey = process.env.API_KEY;
+const api = process.env.API_URL;
+
+var path = require('path')
+const express = require('express')
+
+const app = express()
+const cors = require('cors');
+const bodyParser = require('body-parser');
+app.use(cors()),
+app.use(bodyParser.json())
+
+app.use(express.static('dist'))
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+app.listen(8081, listening)
+
+function listening() {
+    return "Listening on port 8081"
+}
+
+export {listening}
