703b95f52b5cff46f3a2e7f7def95b55504a663f
Abigail Unwin <abigailunwin@gmail.com>
2022-07-16 13:37:19 +0100

    Delete getData.test.js

--- getData.test.js
+++
@@ -1,6 +0,0 @@
-import { getData } from './src/client/js/geonamesAPI';
-describe("Testing the getData function", () => {
-    test('getData resolves rather than rejects', async () => {
-    await expect(getData()).resolves;
-  });
-});
