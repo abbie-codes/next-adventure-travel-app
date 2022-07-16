f4ae5225110863019da644914c494d97acbf193e
Abigail Unwin <abigailunwin@gmail.com>
2022-07-16 13:37:07 +0100

    Delete listening.test.js

--- listening.test.js
+++
@@ -1,6 +0,0 @@
-import {listening} from "./src/server/index.js";
-describe("Testing the server functionality", () => {
-    test("Testing the listening() function", () => {
-        expect(listening).toBeDefined();
-    })
-});
